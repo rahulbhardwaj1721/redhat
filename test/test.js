b6e93cf8d2a8baf4baf2546aa19cf618fd6a3ee8
rahulbhardwaj1721 <rahulbhardwaj1721@gmail.com>
2022-01-26 20:42:02 +0530

    test cases update & logger improvement

---
+++ test/test.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { FASTIFY_PORT, FASTIFY_ADDRESS } = process.env;
+const url = 'http://' + FASTIFY_ADDRESS + ':' + FASTIFY_PORT;
+const requests = require('supertest')(url);
+const config = require('../axiomconfig.json');
+const comicQty = config.XKCD.TOTAL_RECORDS;
+const chai = require('chai');
+const should = chai.should();
+
+describe('Sample unit test case for XKCD service', function () {
+    it('should return random XKCD comic attribute', (done) => {
+        requests.get('/api/xkcd')
+            .expect("Content-type", /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    console.error(res.error);
+                    return done(err);
+                }
+                res.status.should.equal(200);
+                res.body.should.be.a('array');
+                res.body.length.should.equal(comicQty);
+                done();
+            })
+    });
+
+});
